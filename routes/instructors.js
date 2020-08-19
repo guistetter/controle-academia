bf2f01f39ff0fd618a65aac15458cc407cc510cc
guistetter <guilherme_stetter@hotmail.com>
2020-08-19 17:09:46 -0300

    tratando os dados do req.body

--- routes/instructors.js
+++ routes/instructors.js
@@ -7,8 +7,21 @@ exports.post = (req, res) => {
     if (req.body[key]== "")
     return res.send("Preencha todos os campos")
   }
+  let {avatar_url, birth, name, services, gender} = req.body
 
-  data.instructors.push(req.body)
+  birth = Date.parse(birth)
+  const created_at = Date.now()
+  const id = Number(data.instructors.length + 1)
+  
+  data.instructors.push({
+    avatar_url, 
+    birth, 
+    created_at, 
+    id, 
+    name, 
+    services, 
+    gender
+  })
   fs.writeFile("data.json", JSON.stringify(data, null, 3), function(err){  
     if(err) return res.send('Write file Error')
     return res.redirect("/instructors")
