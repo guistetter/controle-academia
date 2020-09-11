9b61aa0d1d6e1817973bd04bbbeca240f59d4a6f
guistetter <guilherme_stetter@hotmail.com>
2020-09-11 15:05:39 -0300

    utilizando moment para manipular data

--- routes/instructors.js
+++ routes/instructors.js
@@ -1,6 +1,7 @@
 const fs = require('fs')
 const data = require("../data.json")
 const {age} = require("../utils")
+const moment = require("moment")
 //show
 exports.show = function(req, res) {
  //req.params
@@ -16,7 +17,7 @@ exports.show = function(req, res) {
    ...foundInstructor,
    age: age(foundInstructor.birth),
    services: foundInstructor.services.split(","),
-   created_at: new Intl.DateTimeFormat("pt-BR").format(new Intl.DateTimeFormat("pt-BR").format(foundInstructor.created_at)),
+   created_at: moment(instructor.created_at).format("dd/MM/yyyy")
  }
 
  return res.render("instructors/show",{
