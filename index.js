d128d209d9fc25fbfe00c8921be1c96d755d9c4d
guistetter <guilherme_stetter@hotmail.com>
2020-09-12 17:59:35 -0300

    criando middleware para method override

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
 const nunjucks = require('nunjucks')
 const express = require("express")
 const server = express()
+const methodOverride = require('method-override')
 const PORT = process.env.PORT || 3001
 const routes = require('./routes/routes')
 
 server.use(express.urlencoded({extended:true}))
+server.use(methodOverride('_method'))
 server.use(routes)
+
 
 server.use(express.static("public"))
 server.set("view engine", "njk")
