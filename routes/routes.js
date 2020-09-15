e80d40e1279ca3fa879347a37d51cf11632e831a
guistetter <guilherme_stetter@hotmail.com>
2020-09-15 11:06:19 -0300

    rotas para members

--- routes/routes.js
+++ routes/routes.js
@@ -2,6 +2,7 @@ const express = require('express')
 const routes = express.Router()
 const instructors = require("../routes/instructors")
 
+//instructors routes
 routes.get('/', function(req, res) {
   return res.redirect('/instructors')
 })
@@ -22,9 +23,27 @@ routes.put("/instructors", instructors.put)
 
 routes.delete("/instructors", instructors.delete)
 
-routes.get('/members', function(req, res){
-  return res.send('members')
+//members routes
+routes.get('/', function(req, res) {
+  return res.redirect('/members')
 })
+
+routes.get('/members',members.index);
+
+routes.get('/members/create',(req,res) => {
+  res.render('members/create.njk')
+})
+
+routes.get('/members/:id', members.show)
+
+routes.get('/members/:id/edit', members.edit)
+
+routes.post("/members",members.post)
+
+routes.put("/members", members.put)
+
+routes.delete("/members", members.delete)
+
 
 
 module.exports = routes
