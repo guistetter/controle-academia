5f26ff55031615b79cba29361162a98ccc8010ca
guistetter <guilherme_stetter@hotmail.com>
2020-09-17 17:01:40 -0300

    transformando logica de paginacao em funcao

--- public/scripts.js
+++ public/scripts.js
@@ -7,28 +7,28 @@ for(item of menuItems){
   }
 }
 
-let totalPages = 20, 
-  selectedPage = 6, 
-  pages = [],
+function pagination(selectedPage, totalPages){
+
+  let pages = [],
   oldPage
 
-for(let currentPage = 1; currentPage <= totalPages; currentPage++){
+  for(let currentPage = 1; currentPage <= totalPages; currentPage++){
 
   const firstAndLastPage = currentPage == 1 || currentPage == totalPages
   const pagesAfterSelectedPage = currentPage <= selectedPage + 2
   const pagesBeforeSelectedPage = currentPage >= selectedPage - 2
 
-  if(firstAndLastPage || pagesBeforeSelectedPage && pagesAfterSelectedPage){
-    
-    if(oldPage && currentPage - oldPage > 2){
-      pages.push('...')
+    if(firstAndLastPage || pagesBeforeSelectedPage && pagesAfterSelectedPage){
+      
+      if(oldPage && currentPage - oldPage > 2){
+        pages.push('...')
+      }
+      if(oldPage && currentPage - oldPage == 2){
+        pages.push(oldPage + 1)
+      }
+      pages.push(currentPage)
+      oldPage = currentPage
     }
-    if(oldPage && currentPage - oldPage == 2){
-      pages.push(oldPage + 1)
-    }
-    pages.push(currentPage)
-    oldPage = currentPage
   }
 }
 
-console.log(pages)
