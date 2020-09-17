fc5d9edcc192583ab33eba45050204b123a53e3b
guistetter <guilherme_stetter@hotmail.com>
2020-09-17 16:47:24 -0300

    Mostrar 2 pgs apos pg atual e 2 pgs depois a atual

--- public/scripts.js
+++ public/scripts.js
@@ -8,17 +8,26 @@ for(item of menuItems){
 }
 
 let totalPages = 20, 
-  selectedPage = 15, 
-  pages = []
+  selectedPage = 6, 
+  pages = [],
+  oldPage
 
 for(let currentPage = 1; currentPage <= totalPages; currentPage++){
 
   const firstAndLastPage = currentPage == 1 || currentPage == totalPages
-  const pagesAfterSelectedPage = selectedPage <= selectedPage + 2
+  const pagesAfterSelectedPage = currentPage <= selectedPage + 2
   const pagesBeforeSelectedPage = currentPage >= selectedPage - 2
 
   if(firstAndLastPage || pagesBeforeSelectedPage && pagesAfterSelectedPage){
+    
+    if(oldPage && currentPage - oldPage > 2){
+      pages.push('...')
+    }
+    if(oldPage && currentPage - oldPage == 2){
+      pages.push(oldPage + 1)
+    }
     pages.push(currentPage)
+    oldPage = currentPage
   }
 }
 
